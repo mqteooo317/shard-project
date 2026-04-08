14a38ab42773fa7b4018b8ac58b676f53f47e363
mqteo <collarmateo0610@gmail.com>
2026-04-07 21:34:02 -0300

    Refactor memcpy_fast for clarity and optimization

--- asm/memcpy_erm.asm
+++ asm/memcpy_erm.asm
@@ -1,16 +1,7 @@
-; memcpy_erms.asm
-; Copia memoria usando REP MOVSB, que en CPUs modernas es óptimo
-; Argumentos:
-;   rdi: destino
-;   rsi: origen
-;   rdx: tamaño
-; Retorna: rdi (destino) por convención
-
 section .text
 global memcpy_fast
 memcpy_fast:
-    ; Usar rep movsb
-    mov rcx, rdx        ; tamaño
+    mov rcx, rdx
     rep movsb
-    mov rax, rdi        ; retornar destino (original)
+    mov rax, rdi
     ret
